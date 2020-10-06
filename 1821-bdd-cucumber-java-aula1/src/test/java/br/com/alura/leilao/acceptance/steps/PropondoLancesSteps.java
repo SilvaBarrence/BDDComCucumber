53c0c62159816b20447299d794a2ab6bca4ab7c5
Gabriel <g.dessilva95@gmail.com>
2020-10-05 22:50:12 -0300

    Escrevendo features com cucumber

---
+++ 1821-bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLancesSteps.java
@@ -0,0 +1,24 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PropondoLancesSteps {
+	
+	@Given("Dado um lance valido")
+	public void dado_um_lance_valido() {
+		throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Quando propoe lance")
+	public void quando_propoe_lance() {
+		throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("Então o lance eh aceito")
+	public void entao_o_lance_eh_aceito() {
+		throw new io.cucumber.java.PendingException();
+	}
+
+}
